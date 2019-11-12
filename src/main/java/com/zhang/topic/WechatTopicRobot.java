1cabec1d0779debcc63ddb3fc31fd97064825692
jiaheng-zhang <jiaheng.zhang@rea-group.com>
2019-11-12 14:52:37 +0800

    init project

---
+++ src/main/java/com/zhang/topic/WechatTopicRobot.java
@@ -0,0 +1,7 @@
+package com.zhang.topic;
+
+public class WechatTopicRobot {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/zhang/topic/WechatTopicRobotTest.java
@@ -0,0 +1,8 @@
+package com.zhang.topic;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class WechatTopicRobotTest {
+}
