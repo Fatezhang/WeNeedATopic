3e274fdb5e48b9625f5ba90475282b940106ef47
jiaheng-zhang <jiaheng.zhang@rea-group.com>
2019-11-12 15:05:40 +0800

    start a Springboot Application

--- src/main/java/com/zhang/topic/WechatTopicRobot.java
+++ src/main/java/com/zhang/topic/WechatTopicRobot.java
@@ -1,7 +1,11 @@
 package com.zhang.topic;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class WechatTopicRobot {
     public static void main(String[] args) {
-
+        SpringApplication.run(WechatTopicRobot.class, args);
     }
 }
